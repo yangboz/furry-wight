e19e781ad572edc99b2334f657e2dab515ff6f44
unknown <yangboz@yzhou24.asiapacific.hpqcorp.net>
2013-05-07 14:50:58 +0800

    CHANGED:applied the fusion matrix and reports function;

--- src/Main.py
+++ src/Main.py
@@ -57,3 +57,9 @@ from sklearn import cross_validation
 train,test,t_train,t_test = cross_validation.train_test_split(data,t,test_size=0.4,random_state=0)
 classifier.fit(train,t_train)#train
 print classifier.score(test, t_test)#test
+# fusion matrix
+from sklearn.metrics import confusion_matrix
+print confusion_matrix(classifier.predict(test),t_test)
+# fusion matrix report
+from sklearn.metrics import classification_report
+print classification_report(classifier.predict(test),t_test,target_names=['setos','versicolor','virginica'])
